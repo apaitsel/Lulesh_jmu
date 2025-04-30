48a823cc93bad6a177123cbcab3ec0830e7b5d7b
Zachary Merkel <merkelzd@dukes.jmu.edu>
2025-04-30 11:10:36 -0400

    deleted unused

--- omp_test/buildserial.sh
+++
@@ -1,6 +0,0 @@
-rm -rf build-serial/
-
-mkdir build-serial
-cd build-serial
-cmake -DCMAKE_BUILD_TYPE=Release -DWITH_MPI=Off -DWITH_OPENMP=Off -DCMAKE_CXX_COMPILER=g++ -DCMAKE_CXX_FLAGS="-fno-inline" ..
-make
